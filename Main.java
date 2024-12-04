e334225760d30c598c2f03a05d87cdff2051386a
irem <iremhverdi@gmail.com>
2024-12-04 19:46:51 +0300

    -hatalar düzeltildi terminate eklendi

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 import java.lang.Thread;
+
 public class Main {
 
     public static final String ANSI_PURPLE = "\u001B[35m";//ANSI codes for colors used for coloring the ASCII
     public static final String ANSI_CYAN = "\u001B[36m"; //Returns to the default color property
-    public static Scanner input = new Scanner(System.in); 
+    public static final String ANSI_GREEN = "\033[32m"; //Green ANSI CODE
+    public static Scanner scanner = new Scanner(System.in,"UTF-8"); 
 
 
     public static void clearConsole(){      //Cleaning Console
@@ -17,42 +19,89 @@ public class Main {
         while(true){
             clearConsole();
             displayLoginScreen();// Display the login screen
-            login();//Login the system
+            int choice= makeChoice();
+
+            switch(choice){
+                case 1:
+                login();//Login the system
+                break;
+
+                case 2:
+                System.exit(0);
+                break;
+                
+                default:
+                System.out.println("Invalid choice. Please choose");
+            }
         }
     }
 
     public static void displayLoginScreen(){
         System.out.println(ANSI_PURPLE);
 
-        String message=("____    __    ____  _______  __        ______   ______   .___  ___.  _______    \n" + //
-                        "\\   \\  /  \\  /   / |   ____||  |      /      | /  __  \\  |   \\/   | |   ____|   \n" + //
-                        " \\   \\/    \\/   /  |  |__   |  |     |  ,----'|  |  |  | |  \\  /  | |  |__      \n" + //
-                        "  \\            /   |   __|  |  |     |  |     |  |  |  | |  |\\/|  | |   __|     \n" + //
-                        "   \\    /\\    /    |  |____ |  `----.|  `----.|  `--'  | |  |  |  | |  |____    \n" + //
-                        "    \\__/  \\__/     |_______||_______| \\______| \\______/  |__|  |__| |_______|   \n" + //
-                        "                                                                                \n" + //
-                        "                                                                                \n" + //
-                        "                                                                                \n" + //
-                        "                                                                                ");
+        String message=("             U _____ u  _        ____   U  ___ u  __  __  U _____ u    \n" + //
+                        " __        __\\| ___\"|/ |\"|    U /\"___|   \\/\"_ \\/U|' \\/ '|u\\| ___\"|/    \n" + //
+                        " \\\"\\      /\"/ |  _|\" U | | u  \\| | u     | | | |\\| |\\/| |/ |  _|\"      \n" + //
+                        " /\\ \\ /\\ / /\\ | |___  \\| |/__  | |/__.-,_| |_| | | |  | |  | |___      \n" + //
+                        "U  \\ V  V /  U|_____|  |_____|  \\____|\\_)-\\___/  |_|  |_|  |_____|     \n" + //
+                        ".-,_\\ /\\ /_,-.<<   >>  //  \\\\  _// \\\\      \\\\   <<,-,,-.   <<   >>     \n" + //
+                        " \\_)-'  '-(_/(__) (__)(_\")(\"_)(__)(__)    (__)   (./  \\.) (__) (__)    ");
+
+        String flower1=("         wWWWw               wWWWw\n" + //
+                        "   vVVVv (___) wWWWw         (___)  vVVVv\n" + //
+                        "   (___)  ~Y~  (___)  vVVVv   ~Y~   (___)\n" + //
+                        "    ~Y~   \\|    ~Y~   (___)    |/    ~Y~\n" + //
+                        "    \\|   \\ |/   \\| /  \\~Y~/   \\|    \\ |/\n" + //
+                        "   \\\\|// \\\\|// \\\\|/// \\\\|//  \\\\|// \\\\\\|///\n" + //
+                        "   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+        
+        String flower2=("  wWWWw               wWWWw\n" +
+                        "   (___) wWWWw         (___)  vVVVv\n" +
+                        "    ~Y~  (___)  vVVVv   ~Y~   (___)    vVVVv\n" +
+                        "    \\|    ~Y~   (___)    |/    ~Y~    (___)\n" +
+                        "    \\ |/   \\| /  \\~Y~/   \\|    \\ |/  \\\\~Y~/\n" +
+                        "   \\\\|// \\\\|// \\\\|/// \\\\|//  \\\\|// \\\\\\|///\n" +
+                        "   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
 
         String[] lines=message.split("\n");//'/n' karakteri sınırlayıcı görev görüyor
 
-        for (String line:lines){//her line için sağ karaktere geçmesi için loop
-            System.out.println(" "+line);
+        for(int i=0;i<40;i++){//sağa kayacağı loop sayısı
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            clearConsole();
+            for (String line:lines){//her line için sağ karaktere geçmesi için loop
+                System.out.println(" ".repeat(i)+line);
+        }
+        }
 
+        System.out.println(ANSI_GREEN);
+        for(int i=0;i<4;i++){
+            clearConsole();
+            System.out.println(flower1);
             try {
                 Thread.sleep(400);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
+            
+            
+            clearConsole();
+            System.out.println(flower2);
+            try {
+                Thread.sleep(400);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-
-        System.out.println("Access the System: Login Required");
+        
+        System.out.println("Access the System: Login the system by press '1'");
+        System.out.println("Terminate the system by press '2'");
         System.out.println(ANSI_CYAN);
         
     }
-
 
     public static void login (){
 
@@ -64,7 +113,7 @@ public class Main {
         while(!isLoggedin){
 
             System.out.print("Enter username: ");
-            username = input.nextLine();
+            username = scanner.nextLine();
             if(auth.authenticateUsername(username)){//if the username is valid
 
                 boolean isValid = false;
@@ -72,7 +121,7 @@ public class Main {
                 while(!isValid){
 
                     System.out.print("Enter password or login to other user by type escape character: ");
-                    String password= input.nextLine();
+                    String password= scanner.nextLine();
                     System.out.println();
                     Employee employee;
                     
@@ -93,7 +142,7 @@ public class Main {
                             System.out.println("You are directed to the Manager Menu");
 
                             try {//upper string will appear for two seconds after that the console will be cleared
-                                Thread.sleep(1200);
+                                Thread.sleep(800);
                                 clearConsole();
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
@@ -123,5 +172,41 @@ public class Main {
         }
         // input.close();
     }   
+    public static int makeChoice(){
+        int choice=0;
+
+        while(choice != 1 && choice != 2) {
+
+            while(!scanner.hasNextInt()){
+                System.out.printf("%nInvalid input. Please enter an integer (1, 2).%n Make a choice: ");
+                scanner.next(); // skip the invalid input 
+            }
+
+            choice = getValidInt();
+
+            if(choice != 1 && choice != 2)
+                System.out.printf("%nInvalid input. Please enter an integer (1, 2).%n Make a choice: ");
+        }
+
+        return choice;
+    }
+
+    public static int getValidInt() {
+
+        int validInput = 0;
+
+        while (true) {
+            if (scanner.hasNextInt()) { 
+                validInput = scanner.nextInt();
+                scanner.nextLine();
+                break;
+            } else {
+                System.out.println("Invalid input. Please enter a valid integer.");
+                scanner.nextLine();
+            }
+        }
+
+        return validInput;
+    }
 }
 
